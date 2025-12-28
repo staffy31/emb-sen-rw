d0eceb1de11b75053cafaa4703545d9e74f33d8a
staffy31 <mounkubito@gmail.com>
2025-12-28 08:09:41 +0200

    feat: add Embassy, Consular, Senegal, Gallery, and Contact components with footer to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
   <div class="min-h-screen bg-linear-to-r from-green-300 via-yellow-200 to-red-300">
     <Navbar />
     <Hero />
+    <Embassy />
+    <Consular />
+    <Senegal />
+    <Gallery />
+    <Contact />
+    <footer class="bg-gray-800 text-white py-4 h-[400px]">
+      <div class="container mx-auto text-center">
+        <p>&copy; 2024 Embassy of Senegal in Rwanda. All rights reserved.</p>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -11,12 +21,22 @@ import 'aos/dist/aos.css';
 import Hero from './components/Hero.vue';
 import Navbar from './components/Navbar.vue';
 import {ref, nextTick, onMounted } from 'vue';
+import Embassy from './components/Embassy.vue';
+import Consular from './components/Consular.vue';
+import Senegal from './components/Senegal.vue';
+import Gallery from './components/Gallery.vue';
+import Contact from './components/Contact.vue';
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    Hero
+    Hero,
+    Embassy,
+    Consular,
+    Senegal,
+    Gallery,
+    Contact
   },
   setup() {
     const loading = ref(true);
---
+++ src/components/Consular.vue
@@ -0,0 +1,16 @@
+<template>
+    <section
+    class=" rounded-xl overflow-hidden hover:shadow-lg transition bg-white h-[400px] m-2 p-2">
+Consular Component
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

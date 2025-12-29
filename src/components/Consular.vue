b9410a9c3ecf6bae0d80b0055b73279f88417914
staffy31 <mounkubito@gmail.com>
2025-12-29 09:25:07 +0200

    Refactor code structure for improved readability and maintainability

--- src/components/Consular.vue
+++ src/components/Consular.vue
@@ -1,7 +1,32 @@
 <template>
-    <section
-    class=" rounded-xl overflow-hidden hover:shadow-lg transition bg-white h-[400px] m-2 p-2">
-Consular Component
+    <section class="overflow-hidden hover:shadow-lg transition bg-gray-300/90 h-[400px] m-2 p-2">
+        <div
+            class="flex flex-col rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-gray-900 md:max-w-xl md:flex-row">
+            <img class="object-cover w-full rounded-base h-89 md:h-95 md:w-full mb-4 md:mb-0" src="/hero/HE_ambassador_Sow.png" alt="">
+            <div class="flex flex-col justify-start p-6">
+                <h5 class="mb-2 text-xl font-medium">Card title</h5>
+                <p class="mb-4 text-base">
+                    Sortant de l’Université Cheikh Anta DIOP de Dakar (UCAD) avec une maitrise en Lettres Modernes en
+                    1992, S.E.M. Doudou SOW a servi dans l’enseignement jusqu’en 2005, année de son admission à l’Ecole
+                    nationale d'administration (ENA) - Section Diplomatie.
+
+                    Il est ensuite affecté au Ministère des Affaires étrangères, à la Direction Europe, où il occupe le
+                    poste de Chef de Division Europe de 2009 à 2010. Il est par la suite affecté à l’Ambassade du
+                    Sénégal à Kingston en tant que Deuxième Conseiller de 2010 à 2011, puis Premier Conseiller de 2011 à
+                    2012. De 2012 à 2015, il occupe également le poste de Premier Conseiller à l’Ambassade du Sénégal à
+                    Brasilia.
+
+                    Entre 2015 et 2016, il devient Conseiller auprès de l’Ambassadeur, Secrétaire général du Ministère
+                    des Affaires étrangères et des Sénégalais de l'extérieur, avant d’être affecté à l’Ambassade du
+                    Sénégal à Séoul, où il a servi comme Ministre-conseiller de 2018 à 2019.
+
+                    Depuis septembre 2019, il est Ambassadeur de la République du Sénégal au Rwanda.
+                </p>
+                <p class="text-xs text-surface/75 dark:text-neutral-300">
+                    Last updated 3 mins ago
+                </p>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -11,6 +36,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/Embassy.vue
+++ src/components/Embassy.vue
@@ -1,7 +1,73 @@
 <template>
-    <section
-    class=" rounded-xl overflow-hidden hover:shadow-lg transition bg-white h-[400px] m-2 p-2">
-Embassy Component
+    <section class="  overflow-hidden hover:shadow-lg transition bg-gray-100/90 h-auto p-2">
+        Embassy Component
+
+
+<div class="grid-cols-1 sm:grid md:grid-cols-3 ">
+  <div
+    class="mx-3 mt-6 flex flex-col rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-gray-900 sm:shrink-0 sm:grow sm:basis-0">
+    <a href="#!">
+
+            <img class="object-cover w-full rounded-base h-64 md:h-95 md:w-full mb-4 md:mb-0"
+                src="/hero/HE_ambassador.png" alt="">
+    </a>
+    <div class="p-6">
+      <h5 class="mb-2 text-xl font-medium leading-tight">Card title</h5>
+      <p class="mb-4 text-base">
+        This is a wider card with supporting text below as a natural
+        lead-in to additional content. This content is a little bit
+        longer.
+      </p>
+    </div>
+    <div
+      class="mt-auto border-t-2 border-neutral-100 px-6 py-3 text-center text-surface/75 dark:border-white/10 dark:text-neutral-300">
+      <small>Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+  <div
+    class="mx-3 mt-6 flex flex-col rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0">
+    <a href="#!">
+      <img
+        class="rounded-t-lg"
+        src="https://tecdn.b-cdn.net/img/new/standard/city/043.webp"
+        alt="Los Angeles Skyscrapers" />
+    </a>
+    <div class="p-6">
+      <h5 class="mb-2 text-xl font-medium leading-tight">Card title</h5>
+      <p class="mb-4 text-base">
+        This card has supporting text below as a natural lead-in to
+        additional content.
+      </p>
+    </div>
+    <div
+      class="mt-auto border-t-2 border-neutral-100 px-6 py-3 text-center text-surface/75 dark:border-white/10 dark:text-neutral-300">
+      <small>Last updated 3 mins ago</small>
+    </div>
+  </div>
+
+  <div
+    class="mx-3 mt-6 flex flex-col rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0">
+    <a href="#!">
+      <img
+        class="rounded-t-lg"
+        src="https://tecdn.b-cdn.net/img/new/standard/city/042.webp"
+        alt="Palm Springs Road" />
+    </a>
+    <div class="p-6">
+      <h5 class="mb-2 text-xl font-medium leading-tight">Card title</h5>
+      <p class="mb-4 text-base">
+        This is a wider card with supporting text below as a natural
+        lead-in to additional content. This card has even longer content
+        than the first to show that equal height action.
+      </p>
+    </div>
+    <div
+      class="mt-auto border-t-2 border-neutral-100 px-6 py-3 text-center text-surface/75 dark:border-white/10 dark:text-neutral-300">
+      <small>Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
     </section>
 </template>
 
@@ -11,6 +77,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
